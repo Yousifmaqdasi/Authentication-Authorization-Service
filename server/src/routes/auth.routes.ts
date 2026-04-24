cc3c72ed702910332abeb225ebf9af869d4942c1
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-04-24 20:35:23 +0200

    chore(routes): update import and function name

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -8,14 +8,14 @@ import {
     resetPassword } 
 from "../controllers/auth.controller";
 
-import { verifyAccessToken } from "../middleware/auth.middleware";
+import { authMiddleware } from "../middleware/auth.middleware";
 import { verifyRefreshToken } from "../middleware/refresh.validation.middleware";
 
 const authRouter = Router()
 
 authRouter.post('/register', register)
 authRouter.post('/login', login)
-authRouter.post('/logout', verifyAccessToken, logout)
+authRouter.post('/logout', authMiddleware, logout)
 authRouter.post('/refresh', verifyRefreshToken, refresh)
 authRouter.post('/forgot-password', forgotPassword)
 authRouter.post('/reset-password/:userId/:token', resetPassword)
