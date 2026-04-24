59e9df0dace67107a5ec35c0aade1a551db1b90d
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-04-24 13:38:23 +0200

    refactor(utils): add clear access and refresh token logic inside generate access and refresh token files

--- server/src/utils/clear.access.token.ts
+++
@@ -1,9 +0,0 @@
-import { Response } from "express"
-
-export const clearAccessToken = (res: Response) => {
-    res.clearCookie("accessToken", {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax"
-    })
-}
--- server/src/utils/clear.refresh.token.ts
+++
@@ -1,9 +0,0 @@
-import { Response } from "express"
-
-export const clearRefreshToken = (res: Response) => {
-    res.clearCookie("refreshToken", {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax"
-    })
-}
