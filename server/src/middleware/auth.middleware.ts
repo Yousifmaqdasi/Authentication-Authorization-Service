a5dbb2f7a1abf11995b8f92d6d1840f13ae0692b
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-04-24 20:26:55 +0200

    refactor(middleware): change middleware function name  and add Role typing to decoded token

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -1,24 +1,32 @@
-import jwt from 'jsonwebtoken'
-import { Response, NextFunction } from 'express'
-import { AuthRequest } from '../types/auth.types'
+import jwt from "jsonwebtoken";
+import { Response, NextFunction } from "express";
+import { AuthRequest } from "../types/auth.types";
+import { Role } from "../types/auth.types";
 
-export const verifyAccessToken = (req: AuthRequest, res: Response, next: NextFunction) => {
+export const authMiddleware = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-    const token = req.cookies.accessToken
-    if(!token) return res.status(401).json({message: "Unauthorized"})
+  const secret = process.env.ACCESS_TOKEN_SECRET;
+  if (!secret) throw new Error("ACCESS_TOKEN_SECRET is not defined");
 
-    const secret = process.env.ACCESS_TOKEN_SECRET
-    if(!secret) throw new Error('ACCESS_TOKEN_SECRET is not defined') 
+  try {
+    const decoded = jwt.verify(token, secret) as {
+      userId: number;
+      role: string;
+    };
 
-    try {
-        const decoded = jwt.verify(token, secret) as {userId: number, role: string}
+    req.user = { id: decoded.userId, role: decoded.role as Role };
 
-        req.user = {id: decoded.userId, role: decoded.role}
-        
-        next()
-    } 
-    catch (error) {
-        console.error("Authentication failed:", error); 
-        return res.status(401).json({error: 'Unauthorized'})
-    }
-}
+    next();
+  } catch (error) {
+    console.error("Authentication failed:", error);
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+};
+
+
