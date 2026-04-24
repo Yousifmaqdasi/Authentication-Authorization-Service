bd815ff35a2ae17443d545f3b15c24207d91009d
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-04-24 13:53:51 +0200

    chore: update generate token files

--- server/src/utils/generate.access.token.ts
+++ server/src/utils/generate.access.token.ts
@@ -1,18 +1,25 @@
-import { Response } from "express"
-import jwt from "jsonwebtoken"
-
+import { Response } from "express";
+import jwt from "jsonwebtoken";
 
 export const accessToken = (res: Response, userId: number, role: string) => {
-    const accessToken = jwt.sign(
-        {userId: userId, role: role },
-        process.env.ACCESS_TOKEN_SECRET!,
-        {expiresIn: "15m"}
-    )
-    
-    res.cookie("accessToken", accessToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 15 * 60 * 1000,
-        sameSite: "lax"
-    })
-}
+  const accessToken = jwt.sign(
+    { userId: userId, role: role },
+    process.env.ACCESS_TOKEN_SECRET!,
+    { expiresIn: "15m" },
+  );
+
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 15 * 60 * 1000,
+    sameSite: "lax",
+  });
+};
+
+export const clearAccessToken = (res: Response) => {
+  res.clearCookie("accessToken", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
+};
--- server/src/utils/generate.refresh.token.ts
+++ server/src/utils/generate.refresh.token.ts
@@ -1,19 +1,25 @@
-import { Response } from "express"
-import jwt from "jsonwebtoken"
-
+import { Response } from "express";
+import jwt from "jsonwebtoken";
 
 export const refreshToken = (res: Response, userId: number) => {
-    const refreshToken = jwt.sign(
-        {userId: userId},
-        process.env.REFRESH_TOKEN_SECRET!,
-        {expiresIn: "7d"}
-    )
+  const refreshToken = jwt.sign(
+    { userId: userId },
+    process.env.REFRESH_TOKEN_SECRET!,
+    { expiresIn: "7d" },
+  );
 
-     res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        sameSite: "lax"
-    })
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    sameSite: "lax",
+  });
+};
 
-}
+export const clearRefreshToken = (res: Response) => {
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
+};
