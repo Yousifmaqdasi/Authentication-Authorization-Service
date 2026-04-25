df1fd22c48dbdd97fa3c47473544486cc4c73bc1
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-04-25 23:17:53 +0200

    feat(routes): add delete user by id and centralize auth middleware

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
-import { getMe, deleteMe, getUsers} from "../controllers/user.controller";
+import { getMe, deleteMe, getUsers } from "../controllers/user.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
 import checkRole from "../middleware/role.middleware";
 import { getUser } from "../controllers/user.controller";
+import { deleteUser } from "../controllers/user.controller";
 
-const usersRouter = Router()
+const usersRouter = Router();
 
-usersRouter.get('/', authMiddleware, checkRole(["admin"]), getUsers)
-usersRouter.get('/:id', authMiddleware ,getUser)
-usersRouter.get('/me', authMiddleware, getMe)
-usersRouter.delete('/me', authMiddleware, deleteMe)
+usersRouter.use(authMiddleware);
 
+usersRouter.get("/", checkRole(["admin"]), getUsers);
+usersRouter.get("/me", getMe);
+usersRouter.delete("/me", deleteMe);
+usersRouter.get("/:id", checkRole(["admin"]), getUser);
+usersRouter.delete("/:id", checkRole(["admin"]), deleteUser);
 
-export default usersRouter
+export default usersRouter;
