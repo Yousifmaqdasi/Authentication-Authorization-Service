a45ab8772930516a88ef7cd590f0fc0d779e3802
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-04-26 17:19:04 +0200

    chore(drizzle): update drizzle config and apply migrations

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/database/schemas/*.ts',
+  out: './src/drizzle',
+  schema: './src/models/*.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
