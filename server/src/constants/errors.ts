0092b2d9cc5e0ed322ac9c833c59ee90366fe23c
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-05-04 00:57:36 +0200

    feat: add errors and time constants

---
+++ server/src/constants/errors.ts
@@ -0,0 +1,3 @@
+export const ERR_INVALID_CREDENTIALS = "Invalid email or password";
+export const ERR_INVALID_TOKEN = "Invalid token";
+export const ERR_EXPIRED_TOKEN = "Expired token";
---
+++ server/src/constants/time.ts
@@ -0,0 +1,3 @@
+export const ONE_HOUR = 60 * 60 * 1000;
+export const THIRTY_MIN = 30 * 60 * 1000;
+export const FIFTEEN_MIN = 15 * 60 * 1000;
